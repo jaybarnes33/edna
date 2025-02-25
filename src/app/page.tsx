0db690d01e5d1661ec636eea004a85be979723d9
John Barnes <ohenesetwumasi@gmail.com>
2025-02-25 20:40:07 +0000

    feat: seo for link previews

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,6 +15,42 @@ export const metadata = {
     "UI/UX",
     "Product Design",
   ],
+  openGraph: {
+    title: "Emerald Edna Quarshie - UX Designer & Software Designer",
+    description:
+      "Professional UX Designer and Software Designer specializing in creating stylish, modern websites, web services and online stores.",
+    url: "https://edna-kappa.vercel.app/",
+    siteName: "Emerald Edna Portfolio",
+    images: [
+      {
+        url: "/images/edna.png",
+        width: 1200,
+        height: 630,
+        alt: "Emerald Edna Quarshie",
+      },
+    ],
+    locale: "en_US",
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Emerald Edna Quarshie - UX Designer & Software Designer",
+    description:
+      "Professional UX Designer and Software Designer specializing in creating stylish, modern websites, web services and online stores.",
+
+    images: ["/images/edna.png"],
+  },
+  robots: {
+    index: true,
+    follow: true,
+    googleBot: {
+      index: true,
+      follow: true,
+      "max-video-preview": -1,
+      "max-image-preview": "large",
+      "max-snippet": -1,
+    },
+  },
 };
 
 const page = () => {
